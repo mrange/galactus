64af733b384a50fe579e8d434517f5267d3869a3
mrange <marten_range@hotmail.com>
2017-11-29 22:44:58 +0100

    More tweaks

---
+++ src/Galactus.Core/Unit.cs
@@ -0,0 +1,24 @@
+﻿namespace Galactus.Core
+{
+  using System;
+
+  public sealed class Unit : IEquatable<Unit>
+  {
+    public readonly static Unit Zero = new Unit ();
+
+    public override int GetHashCode()
+    {
+      return 0x55555555;
+    }
+
+    public override bool Equals(object obj)
+    {
+      return obj != null && obj is Unit;
+    }
+
+    public bool Equals(Unit other)
+    {
+      return other != null;
+    }
+  }
+}
